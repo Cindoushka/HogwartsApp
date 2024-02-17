38a31a3f529d1fc51129ab1735639e566ab9748e
Cindoushka <cindie-baj@live.fr>
2024-02-17 01:06:10 +0100

    update of class and struct, modifications on the quiz view

---
+++ HogwartsApp/Quiz/TestQuiz.swift
@@ -0,0 +1,18 @@
+//
+//  TestQuiz.swift
+//  HogwartsApp
+//
+//  Created by apprenant72 on 16/02/2024.
+//
+
+import SwiftUI
+
+struct TestQuiz: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestQuiz()
+}
